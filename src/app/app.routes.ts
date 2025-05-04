eaffbc8b9c849cc60ee77d2c2d53df6836b34164
chun <yanchun.chang5@gmail.com>
2025-05-04 20:39:43 +0200

    feat: add routing and components for navigation and sudoku board

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { SudokuBoardComponent } from './components/sudoku-board/sudoku-board.component';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:'',
+    component: WelcomePageComponent
+  },
+  {
+    path:'sudoku',
+    component: SudokuBoardComponent
+  },];
