3e5f3ecd9c9e14578d67654179df966f532f27b2
chun <yanchun.chang5@gmail.com>
2025-05-06 20:22:21 +0200

    feat: implement Sudoku generation logic and data parsing in SudokuService

--- src/app/services/sudoku.service.ts
+++ src/app/services/sudoku.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { getSudoku } from 'sudoku-gen';
+import { SudokuDataParsed, SudokuDataRaw } from '../models/sudoku-data';
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +8,37 @@ import { getSudoku } from 'sudoku-gen';
 export class SudokuService {
   initialBoard: number[][] = [];
   solvedBoard: number[][] = [];
-  
-  sudokuEasy = getSudoku('easy');
 
   constructor() {
-        console.log(this.sudokuEasy);
+  }
+
+  generateSudoku(level: any) {
+    const sudokuDataRaw: SudokuDataRaw = getSudoku(level);
+    const SudokuDataParsed: SudokuDataParsed =  this.parsedSudokuData(sudokuDataRaw);
+    
+  }
+
+  private parsedSudokuData(sudokuDataRaw: SudokuDataRaw): SudokuDataParsed {
+    const parsedSudokuData = {
+      puzzle: this.parseSudokuString(sudokuDataRaw.puzzle),
+      solution: this.parseSudokuString(sudokuDataRaw.solution),
+      difficulty: sudokuDataRaw.difficulty
+    }
+    console.log(parsedSudokuData)
+    return parsedSudokuData;
+  }
+
+  private parseSudokuString(sudokuString: string): number[][] {
+    const sudokuArray: number[][] = [];
+    for (let i = 0; i < 9; i++) {
+      const row: number[] = [];
+      for (let j = 0; j < 9; j++) {
+        const index = i * 9 + j
+        const char = sudokuString[index]
+        row.push(char === '-' ? 0 : Number(char))
+      }
+      sudokuArray.push(row);
+    }
+    return sudokuArray;
   }
 }
