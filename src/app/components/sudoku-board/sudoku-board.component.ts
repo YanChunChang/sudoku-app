812b46f22da7be2b8103fd4bd27a74e7e58ac4e2
chun <yanchun.chang5@gmail.com>
2025-05-07 16:37:43 +0200

    feat: add input validation and key restriction for Sudoku cells

--- src/app/components/sudoku-board/sudoku-board.component.ts
+++ src/app/components/sudoku-board/sudoku-board.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterModule } from '@angular/router';
-import { ReactiveFormsModule, FormBuilder, FormArray, FormControl, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { SudokuService } from '../../services/sudoku.service';
 
 @Component({
@@ -19,14 +19,14 @@ export class SudokuBoardComponent {
   }
 
   ngOnInit() {
-    //notwendig für route wechseln
+    //subscribe necessary for route changing
     this.route.paramMap.subscribe(params => {
       const level = params.get('level')
       this.sudokuService.generateSudoku(level);
       this.initialBoard = this.sudokuService.initialBoard;
       this.solvedBoard = this.sudokuService.solvedBoard;
 
-      // Warten bis Boards geladen sind
+      // wait til board loads
       const boardArray = this.createBoard();
       this.form = this.fb.group({
         board: this.fb.array(boardArray)
@@ -38,14 +38,14 @@ export class SudokuBoardComponent {
     })
   }
 
-  //initialboard has 0 value, in Formcontrol is null for showing space in html
+  //initialboard has value 0, in formcontrol 0 is present as null to show space in html
   createBoard(): FormArray[] {
     const board: FormArray[] = [];
     for (let i = 0; i < 9; i++) {
       const row: FormArray = this.fb.array([]);
       for (let j = 0; j < 9; j++) {
         const value = this.initialBoard[i][j];
-        const cell: FormControl = value === 0 ? this.fb.control(null) : this.fb.control(value);
+        const cell: FormControl = value === 0 ? this.fb.control(null, [Validators.required, Validators.pattern(/[1-9]/)]) : this.fb.control(value);
         row.push(cell);
       }
       board.push(row);
@@ -82,11 +82,18 @@ export class SudokuBoardComponent {
     } else {
       return this.onInputChanged(row, col) ? "correct" : "incorrect";
     }
-
   }
 
-  isInitialCell(row: number, col: number): boolean {
-    return this.initialBoard[row][col] !== 0
+  //Validator in Angular only check the value like e.g. control.invalid 
+  //todo chinese still can be typed in cell because of composition event
+  onKeyDown(event: KeyboardEvent){
+    const allowedKeys =[
+      '1', '2', '3', '4', '5', '6', '7', '8', '9','Backspace', 'Delete',
+    'ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown','Tab'];
+    
+    if (!allowedKeys.includes(event.key)){
+      event.preventDefault();
+    }
   }
 
   ngOnDestroy(): void {
