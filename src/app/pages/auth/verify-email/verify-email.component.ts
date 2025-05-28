0e0eec0944b00ceba4105f7dee5185dc0817a55f
chun <yanchun.chang5@gmail.com>
2025-05-28 02:41:29 +0200

    feat: add email resend functionality with success and error messages in multiple languages

--- src/app/pages/auth/verify-email/verify-email.component.ts
+++ src/app/pages/auth/verify-email/verify-email.component.ts
@@ -4,20 +4,23 @@ import { AuthService } from '../../../services/auth/auth.service';
 import { TranslateService } from '@ngx-translate/core';
 import { ButtonModule } from 'primeng/button';
 import { TranslateModule } from '@ngx-translate/core';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-verify-email',
   standalone: true,
-  imports: [RouterModule, ButtonModule, TranslateModule],
+  imports: [RouterModule, ButtonModule, TranslateModule, ToastModule],
   templateUrl: './verify-email.component.html',
-  styleUrl: './verify-email.component.scss'
+  styleUrl: './verify-email.component.scss',
+  providers: [MessageService]
 })
 export class VerifyEmailComponent {
   message = '';
   error = '';
 
-  constructor(private authService: AuthService, private translate: TranslateService) { }
-
+  constructor(private authService: AuthService, private translate: TranslateService, private messageService: MessageService) { }
+  
   resendVerificationEmail(): void {
     const email = localStorage.getItem('email');
     if (!email) {
@@ -28,10 +31,20 @@ export class VerifyEmailComponent {
         next: (res) => {
           this.message = this.translate.instant(res.messageKey);
           this.error = '';
+          this.messageService.add({
+            severity: 'success',
+            summary: this.message,
+            detail: this.translate.instant('VERIFY_EMAIL.RESEND.SUCCESS_TEXT')
+          });
         },
         error: (err) => {
           this.error = this.translate.instant(err.error.messageKey);
           this.message = '';
+          this.messageService.add({
+            severity: 'error',
+            summary: this.translate.instant('VERIFY_EMAIL.RESEND.ERROR'),
+            detail: this.error
+          });
         }
       });
     }
