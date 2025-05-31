0a57c6296be870add1fa9c65d0743ccd930f3e53
chun <yanchun.chang5@gmail.com>
2025-05-31 19:58:58 +0200

    feat: enhance recover email component with email sent confirmation, error handling, and translation support

--- src/app/pages/auth/recover-email/recover-email.component.ts
+++ src/app/pages/auth/recover-email/recover-email.component.ts
@@ -5,24 +5,31 @@ import { AuthService } from '../../../services/auth/auth.service';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { Router, RouterModule } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-recover-email',
   standalone: true,
-  imports: [ReactiveFormsModule, TranslateModule, CommonModule, ButtonModule, RouterModule],
+  imports: [ReactiveFormsModule, TranslateModule, CommonModule, ButtonModule, RouterModule, ToastModule],
   templateUrl: './recover-email.component.html',
-  styleUrl: './recover-email.component.scss'
+  styleUrl: './recover-email.component.scss',
+  providers: [MessageService]
 })
 export class RecoverEmailComponent {
-  form! : FormGroup;
+  form!: FormGroup;
   isSubmitted: boolean = false;
+  emailSent: boolean = false;
+  emailNotFound : boolean = false;
+  error: string = '';
 
   constructor(
     private formBuilder: FormBuilder,
     private translate: TranslateService,
     private authService: AuthService,
-    private router: Router
-  ){}
+    private router: Router,
+    private messageService: MessageService
+  ) { }
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
@@ -30,19 +37,29 @@ export class RecoverEmailComponent {
     });
   }
 
-  recover(){
-    if(this.form.invalid) return;
+  recover() {
+    if (this.form.invalid) return;
 
     this.isSubmitted = true;
     const form = this.form.getRawValue();
 
     this.authService.recoverEmail(form.email).subscribe({
-      next: (res) =>{
-
-        this.router.navigate(['/login/reset-password'])
+      next: (res) => {
         this.isSubmitted = false;
+        this.emailSent = true;
       },
-      error: (err) =>{
+      error: (err) => {
+        const messageKey = err.error.messageKey;
+
+        this.emailNotFound = messageKey === 'FORGOT_PASSWORD.EMAIL_NOT_FOUND';
+        this.error = this.translate.instant(err.error.messageKey);
+
+        this.messageService.add({
+          severity: 'error',
+          summary: this.translate.instant('FORGOT_PASSWORD.ERROR'),
+          detail: this.translate.instant(messageKey)
+        });
+
         this.isSubmitted = false;
       }
     })
