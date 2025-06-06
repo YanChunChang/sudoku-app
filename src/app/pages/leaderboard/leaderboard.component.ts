801dddc58d72c6d687a4ac09de914b8a1c0743c2
chun <yanchun.chang5@gmail.com>
2025-06-06 16:26:15 +0200

    feat: enhance leaderboard component with global filtering, sortable columns, and improved layout

--- src/app/pages/leaderboard/leaderboard.component.ts
+++ src/app/pages/leaderboard/leaderboard.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
@@ -8,19 +8,31 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { ActivatedRoute } from '@angular/router';
 import { LeaderboardService } from '../../services/leaderboard/leaderboard-service.service';
 import { formattedTime } from '../../utils/utils';
+import { InputTextModule } from 'primeng/inputtext';
+import { SelectModule } from 'primeng/select';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
 
 
 @Component({
   selector: 'app-leaderboard',
   standalone: true,
-  imports: [TableModule, ButtonModule, CommonModule, FormsModule, MultiSelectModule, TranslateModule],
+  imports: [TableModule, ButtonModule, CommonModule, FormsModule, MultiSelectModule, TranslateModule, InputTextModule, SelectModule, IconFieldModule, InputIconModule],
   templateUrl: './leaderboard.component.html',
   styleUrl: './leaderboard.component.scss'
 })
 export class LeaderboardComponent {
+  @ViewChild('dt2') dt2: any;
   players: any[] = [];
-  cols: { field: string, header: string; }[] = [];
+  cols: { field: string, header: string; sortable: boolean}[] = [];
   selectedColumns: { field: string; header: string; }[] = [];
+  levelOrder = {
+    'easy': 1,
+    'medium': 2,
+    'hard': 3,
+    'expert': 4
+  };
+  
 
   constructor(
     private route: ActivatedRoute,
@@ -43,20 +55,24 @@ export class LeaderboardComponent {
       console.log('Loading leaderboard with:', playerMode, playMode, level, limit);
 
       this.leaderboardService.getLeaderboard(playerMode, playMode, level, limit).subscribe(data => {
-        this.players = data;
-        console.log(this.players);
+        console.log(data)
+        this.players = data.map((player, index) => ({
+          ...player,
+          ranking: index + 1
+        }));
       });
 
     });
 
 
     this.cols = [
-      { field: 'nickname', header: this.translate.instant('LEADERBOARD.NAME') },
-      { field: 'playerMode', header: this.translate.instant('LEADERBOARD.PLAYMODE') },
-      { field: 'playMode', header: this.translate.instant('LEADERBOARD.GAMETYPE') },
-      { field: 'level', header: this.translate.instant('LEADERBOARD.LEVEL') },
-      { field: 'time', header: this.translate.instant('LEADERBOARD.PLAYINGTIME') },
-      { field: 'date', header: this.translate.instant('LEADERBOARD.DATE') },
+      { field: 'nickname', header: this.translate.instant('LEADERBOARD.NAME'), sortable: true },
+      { field: 'userType', header: this.translate.instant('LEADERBOARD.USER'), sortable: true },
+      { field: 'playerMode', header: this.translate.instant('LEADERBOARD.PLAYMODE'), sortable: true },
+      { field: 'playMode', header: this.translate.instant('LEADERBOARD.GAMETYPE'), sortable: true },
+      { field: 'level', header: this.translate.instant('LEADERBOARD.LEVEL'), sortable: true },
+      { field: 'time', header: this.translate.instant('LEADERBOARD.PLAYINGTIME'), sortable: true },
+      { field: 'date', header: this.translate.instant('LEADERBOARD.DATE'), sortable: true },
     ];
     this.selectedColumns = [...this.cols];
   }
@@ -64,4 +80,10 @@ export class LeaderboardComponent {
   formattTime(seconds: number): string{
     return formattedTime(seconds);
   }
+
+  filterGlobal(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.dt2.filterGlobal(value, 'contains');
+  }
+  
 }
