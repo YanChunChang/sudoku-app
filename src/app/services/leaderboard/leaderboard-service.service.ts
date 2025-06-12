e47f3dbc5af9537e744d6b708ecc39b7612c4a91
chun <yanchun.chang5@gmail.com>
2025-06-12 14:50:42 +0200

    fix: update leaderboard API URL to use environment configuration

--- src/app/services/leaderboard/leaderboard-service.service.ts
+++ src/app/services/leaderboard/leaderboard-service.service.ts
@@ -3,12 +3,13 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ScoreData } from '../../models/userData';
 import { AuthService } from '../auth/auth.service';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LeaderboardService {
-  private apiUrl = 'http://localhost:3000/api/leaderboard';
+  private apiUrl = `${environment.apiUrl}/leaderboard`;
   constructor(private http: HttpClient, private authService: AuthService) { }
 
   submitScore(scoreData: ScoreData, isLoggedIn: boolean): Observable<any> {
